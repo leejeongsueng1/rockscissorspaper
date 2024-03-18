b483256cc1371d4484c01a515800cca298ec40ee
leejeongseung1 <jki1256@hanmail.net>
2024-03-18 22:04:26 +0900

    대체이미지 반환 코드 작성

--- src/component/Box.jsx
+++ src/component/Box.jsx
@@ -1,12 +1,26 @@
 import React from "react";
 export const Box = (props) => {
-    return (
+
+    if(props.img){
+        return (
         <div  className="box">
             <h1 className={props.title}>{props.title}</h1>
             <hr/>
-            <img className='item-img' src={props && props.img} alt="No Pics"/>
+            <img className='item-img' src={props && props.img} alt=""/>
             <hr/>
             <h2 className="state">{props.winstate}</h2>
         </div>
     )
+    }else{
+        return (
+        <div  className="box">
+            <h1 className={props.title}>{props.title}</h1>
+            <hr/>
+            <div className='nullBox'></div>
+            <hr/>
+            <h2 className="state">{props.winstate}</h2>
+        </div>
+    )
+    }
+
 }
