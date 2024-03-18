22be9d8cefac7516dca2320a599ea4626c7d960f
leejeongseung1 <jki1256@hanmail.net>
2024-03-18 21:20:13 +0900

    첫번째 프로젝트: 가위바위보 게임 1강~4강 4강까지의 결과물 도메인 제출

--- src/App.js
+++ src/App.js
@@ -1,24 +1,69 @@
-import logo from './logo.svg';
 import './App.css';
+import {Box} from "./component/Box";
+import {useState} from "react";
+
+
+const choice = {
+    rock: '../images/rock.jpg',
+    scissors: '../images/scissors.jpg',
+    paper: '../images/paper.png'
+}
+const winner = {
+     win : "WIN"
+        , lose : "LOSE"
+        , draw : "DRAW"
+}
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+
+  /*
+  TODO: 1. 박스 2개 (타이틀, 사진, 결과)
+        2. 가위바위보 버튼 존재
+        3. 나: 버튼을 클릭하면 클릭한 값이 박스에 보임
+        4. 컴퓨터는 랜덤하게 아이템 선택이 된다.
+        5. 3,4번의 결과를 가지고 누가 이겼는지 승패를 따진다.
+        6. 승패 여부에 따라 테두리 색이 바뀐다: 이기면 초록, 지면 빨강, 비기면 검정
+  */
+
+    const [userSelect, setUserSelect] = useState(null);
+    const [npcSelect, setNpcSelect] = useState(null);
+
+    function getRandomInt(min, max) {
+      min = Math.ceil(min);
+      max = Math.floor(max);
+      return Math.floor(Math.random() * (max - min)) + min; //최댓값은 제외, 최솟값은 포함
+    }
+    const play = (userChoice) =>{
+        switch (getRandomInt(0, 3)){
+            case 0:
+                console.log('select 0');
+                setNpcSelect(choice['rock']);
+                break;
+            case 1:
+                console.log('select 1');
+                setNpcSelect(choice['scissors']);
+                break;
+            case 2:
+                console.log('select 2');
+                setNpcSelect(choice['paper']);
+                break;
+        }
+        setUserSelect(choice[userChoice]);
+    }
+
+    return (
+        <>
+        <div className="main">
+            <Box title="YOU" img={userSelect} winstate={winner.win}></Box>
+            <Box title="NPC" img={npcSelect} winstate={winner.win}></Box>
+        </div>
+        <div className="main">
+            <button className="userButton" onClick={()=>play("scissors")}><img className='icon' src="../images/scissors_icon.png"/></button>
+            <button className="userButton" onClick={()=>play("rock")}><img className='icon' src="../images/rock_icon.png"/></button>
+            <button className="userButton" onClick={()=>play("paper")}><img className='icon' src="../images/paper_icon.png"/></button>
+        </div>
+        </>
+
   );
 }
 
---
+++ src/component/Box.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+export const Box = (props) => {
+    return (
+        <div  className="box">
+            <h1 className={props.title}>{props.title}</h1>
+            <hr/>
+            <img className='item-img' src={props && props.img}/>
+            <hr/>
+            <h2 className="state">{props.winstate}</h2>
+        </div>
+    )
+}
